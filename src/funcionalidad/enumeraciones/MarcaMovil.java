0ab371645dc22aad834fcc0be26da80724ace6c7
Miguel Ángel López Moyano <i02lomom@gmail.com>
2016-05-17 16:38:20 +0200

    Create MarcaMovil.java

---
+++ src/funcionalidad/enumeraciones/MarcaMovil.java
@@ -0,0 +1,30 @@
+package funcionalidad.enumeraciones;
+
+/**
+ * Enumeración MarcaMovil. Contiene las marcas de móviles.
+ * 
+ * @author Miguel Angel López Moyano
+ * @version 1.0
+ */
+public enum MarcaMovil {
+	/**
+	 * Marca Samsung
+	 */
+	SAMSUNG,
+	/**
+	 * Marca Apple
+	 */
+	APPLE,
+	/**
+	 * Marca Bq
+	 */
+	BQ,
+	/**
+	 * Marca Sony
+	 */
+	SONY,
+	/**
+	 * Marca Huawei
+	 */
+	HUAWEI;
+}
